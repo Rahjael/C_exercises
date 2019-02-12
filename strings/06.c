dee13ed284f39b460c19219f3a3a96636b9a8c22
Rahjael <surfer.vortex@gmail.com>
2019-02-12 15:56:01 +0100

    modifiche varie

--- strings/06.c
+++ strings/06.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 
 
+int CountString(char *);
+
+
 int main(){
 
 /*
 Write a program in C to compare two string without 
 using string library functions.
-
 */
 
 
+    char string1[50], string2[50];
+
+    printf("Inserire due stringhe:\n");
+    scanf("%s", string1);
+    scanf("%s", string2);
+
+    int str_length1 = CountString(string1);
+    int str_length2 = CountString(string2);
 
 
-
-
-
-
-
+    if (str_length1 > str_length2){
+        printf("string 1 is longer than string2: %d > %d", str_length1, str_length2);
+    }
+    else if (str_length1 < str_length2)
+    {
+        printf("string 2 is longer than string1: %d > %d", str_length2, str_length1);
+    }
+    else
+    {
+        printf("The strings are of equal length: %d = %d", str_length1, str_length2);
+    }
+    
     return 0;
+}
+
+
+int CountString(char * string){
+
+    int count = 0;
+
+    for (int i=0; string[i] != '\0'; i++){
+        count++;
+        printf("%s - Size of string: %d\n", string, count);
+    }
+
+    for (int i=0; i<10; i++){
+        printf("String: %c\n", string[i]);
+    }
+
+    return count;
 }
---
+++ strings/07.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/*
+Write a program in C to count total number of 
+alphabets, digits and special characters in a string.
+*/
+
+int CountStuff(char * string, int * ptr_alpha, int * ptr_digit, int * ptr_special){
+
+
+    for (int i=0; string[i]!= '\0'; i++){
+        
+        if (string[i] >= 65 && string[i] <= 90 || string[i] >= 97 && string[i] <= 122){
+            *ptr_alpha += 1;
+        }
+        else if (string[i] >= 48 && string[i] <= 57){
+            *ptr_digit += 1;
+        }
+        else if (string[i] >= 32 && string[i] <= 47 || string[i] >= 58 && string[i] <= 64 || string[i] >= 91 && string[i] <= 96 || string[i] >= 123 && string[i] <= 126){
+            *ptr_special += 1;
+        }
+        else
+        {
+            printf("Carattere non riconosciuto. Errore.");
+            return -1;
+        }
+        
+    }
+
+    return 0;
+}
+
+int main(){
+
+    char string[50];
+
+    int alpha = 0;
+    int digit = 0;
+    int special = 0;
+
+    int * ptr_alpha = &alpha;
+    int * ptr_digit = &digit;
+    int * ptr_special = &special;
+
+
+    printf("Inserisci una stringa:");
+    gets(string);
+
+    int esito = CountStuff(string, &alpha, &digit, &special);
+
+    if (esito == 0){
+        printf("\n\nConteggio completato.\n\n");
+        
+        printf("Stringa inserita: %s\n", string);
+        printf("Alfabetici: %d\n", alpha);
+        printf("Numerici: %d\n", digit);
+        printf("Special: %d\n", special);
+
+    }
+    else if (esito == -1){
+        printf("Errore nel conteggio");
+    }
+    else
+    {
+        printf("Unexpected error:");
+    }
+    
+    return 0;
+}
