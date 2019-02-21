12d1dbc6c77de7512610043ab1017eb510109af8
Rahjael <surfer.vortex@gmail.com>
2019-02-21 12:46:31 +0100

    modifiche varie

--- fuel_tracker/test_datetime.c
+++ fuel_tracker/test_datetime.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <time.h>
+#include <conio.h>
 
 
 int main(){
 
-    time_t rawtime = time(NULL);
+    struct tm *local, *gm;
+    time_t t;
 
-    if (rawtime == -1){
-        puts("The time() function failed");
-        return 1;
-    }
+    t = time(NULL);
+    local = localtime(&t);
+    printf("Local time and date using localtime function\n");
+    printf("time --> %d:%d:%d\n", local->tm_hour, local->tm_min, local->tm_sec);
+    printf("date --> %d/%d/%d\n", local->tm_year+1900, local->tm_mon+1, local->tm_mday);
+    printf("This is the %d day of this year\n", local->tm_yday+1);
+    printf("This is the %d day of this week\n", local->tm_wday+1);
 
-    struct tm * ptm = localtime(&rawtime);
+    gm = gmtime(&t);
+    printf("UTC time and date using gmtime function\n");
+    printf("time --> %d : %d : %d\n", gm->tm_hour, gm->tm_min, gm->tm_sec);
+    printf("date --> %d / %d / %d\n", gm->tm_year+1900, gm->tm_mon+1, gm->tm_mday);
+    printf("This is the %d day of this year\n", gm->tm_yday+1);
+    printf("This is the %d day of this week\n", gm->tm_wday);
 
-    if (ptm == NULL){
-        puts("The localtime() function failed");
-        return 1;
-    }
 
-    printf("The time is: %02d:%02d:%02d\n", ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
-    
 
+    getchar();
     return 0;
 
 }
