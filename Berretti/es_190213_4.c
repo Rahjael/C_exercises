24c6f00aacf4fd52db918c7ab2fd88436f1018e3
Rahjael <surfer.vortex@gmail.com>
2019-07-17 16:13:12 +0200

    bla

--- Berretti/es_190213_4.c
+++ Berretti/es_190213_4.c
@@ -55,46 +55,27 @@ void reverse_records(struct list * list)
         }
         // a questo punto penultimate_index dovrebbe essere l'indice del penultimo elemento
 
-        // swap values
-        // salvo il valore del primo elemento in temp
-        temp = list->buffer[head_index].value;
-
-        // metto il valore dell'ultimo elemento nel primo elemento
-        list->buffer[head_index].value = list->buffer[(list->buffer[penultimate_index].next)].value;
-        
-        // metto il valore di temp nell'ultimo elemento
-        list->buffer[(list->buffer[penultimate_index].next)].value = temp;
-        // fine swap values
-
-
-        // cambiati i valori, cambio gli indici
-
+        // cambio gli indici
         // se è la prima iterazione, cambio list->first con l'indice dell'ultimo elemento che adesso è primo
         if(counter == 0)
         {
             list->first = list->buffer[penultimate_index].next;
         }
-
-
-        // cambio il next dell'ultimo elemento per farlo puntare al penultimo (inverto l'ordine)
-        list->buffer[(list->buffer[penultimate_index].next)].next = penultimate_index;
-
-        // setto come ultimo il penultimo elemento
-        list->buffer[penultimate_index].next = list->size;
-
-        // faccio avanzare l'indice head all'elemento successivo
-        head_index = list->buffer[head_index].next;
-
-        // riporto penultimate_index al pari di head
-        penultimate_index = head_index;
+        
+        list->buffer[(list->buffer[penultimate_index].next)].next = penultimate_index; // cambio il next dell'ultimo elemento per farlo puntare al penultimo (inverto l'ordine)
+        list->buffer[penultimate_index].next = list->size; // setto come ultimo il penultimo elemento
+        head_index = list->buffer[head_index].next; // faccio avanzare l'indice head all'elemento successivo
+        penultimate_index = head_index; // riporto penultimate_index al pari di head
 
         counter++; // aumento counter per capire dove sono
     }
 
-    // torno all'ex primo elemento (che ora è ultimo) e gli do il next appropriato
+    // torno all'ex primo elemento (che ora è ultimo) e gli do il next appropriato per segnalare il fine lista
     list->buffer[original_first].next = list->size;
 
 }
 
+
+// fine esercizio
 
 
